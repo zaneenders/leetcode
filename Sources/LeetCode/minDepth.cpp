72cec3e55c076e21a492b553936bea317fbbf1f5
zane <39070793+zaneenders@users.noreply.github.com>
2025-05-13 14:49:24 -0600

    minDepth

--- Sources/LeetCode/minDepth.cpp
+++ Sources/LeetCode/minDepth.cpp
@@ -2,5 +2,31 @@
 
 int Solution::minDepth(TreeNode *root)
 {
-  return 0;
+  if (root == nullptr)
+  {
+    return 0;
+  }
+  int left = 0;
+  int right = 0;
+  if (root->left != nullptr)
+  {
+    left = minDepth(root->left) + 1;
+  }
+  if (root->right != nullptr)
+  {
+    right = minDepth(root->right) + 1;
+  }
+  if (root->right == nullptr && root->left != nullptr)
+  {
+    return left;
+  }
+  if (root->left == nullptr && root->right != nullptr)
+  {
+    return right;
+  }
+  if (root->left == nullptr && root->right == nullptr)
+  {
+    return 1;
+  }
+  return std::min(left, right);
 }
