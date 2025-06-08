be097b99c94a7a537b550d306229b81d1b5f8ef1
zane <39070793+zaneenders@users.noreply.github.com>
2025-06-08 12:57:57 -0600

    lexicalOrder hack.

--- Sources/LeetCode/lexicalOrder.swift
+++ Sources/LeetCode/lexicalOrder.swift
@@ -1,5 +1,9 @@
 extension Solution {
   public func lexicalOrder(_ n: Int) -> [Int] {
-    []
+    var range: [Int] = Array(1...n)
+    range.sort { left, right in
+      "\(left)" < "\(right)"  // bad allocations
+    }
+    return range
   }
 }
--- Tests/LeetCodeTests/lexcialOrder.swift
+++ Tests/LeetCodeTests/lexcialOrder.swift
@@ -3,10 +3,17 @@ import Testing
 
 @Suite
 struct LexicalOrder {
-  @Test func one() {
+  @Test func exampleOne() {
     let sol = Solution()
     let result = sol.lexicalOrder(13)
     let expected = [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9]
+    #expect(result == expected)
+  }
+
+  @Test func exampleTwo() {
+    let sol = Solution()
+    let result = sol.lexicalOrder(2)
+    let expected = [1, 2]
     #expect(result == expected)
   }
 }
